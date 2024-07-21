961e99a6b95785a48fbddc1bed67628f842333a7
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-21 16:32:09 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,23 +1,31 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
+const isGoodPath = (data: Buffer): boolean => {
+  const req = data.toString();
+  const parts = req.split("\r\n");
+  const method = parts[0];
+  const path = method.split(" ")[1];
+  return path === "/";
+};
+
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
 
   socket.on("data", (data) => {
-    const req = data.toString();
-    console.log(req);
-
-    const resp = `HTTP/1.1 200 OK\r\n\r\n`;
+    const goodPath = isGoodPath(data);
+    const resp = goodPath
+      ? `HTTP/1.1 200 OK\r\n\r\n`
+      : `HTTP/1.1 404 Not Found\r\n\r\n`;
     socket.write(resp);
 
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on http://localhost:4221");
+});
