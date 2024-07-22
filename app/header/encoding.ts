7e3c5c9a880ae9975a6b5648101d6801abce8227
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-22 15:02:05 +0530

    pass stage

--- app/header/encoding.ts
+++ app/header/encoding.ts
@@ -3,12 +3,11 @@ import * as zlib from "zlib";
 export const gzipEncode = async (body: string): Promise<Buffer> => {
   const compressedBody = await new Promise<Buffer>((resolve, reject) => {
     zlib.gzip(body, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
+      if (err) reject(err);
+      else resolve(result);
     });
   });
+
+  console.log("Compressed body length:", compressedBody, body);
   return compressedBody;
 };
